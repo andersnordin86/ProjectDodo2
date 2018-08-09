06482b7fc5eacf8dabee3417569959f11f4c2f3f
Anders Nordin <andersnordin86@gmail.com>
2018-08-09 11:08:08 +0200

    added members class and added disapperaing content

--- src/main/java/com/example/ProjectDodo/LoginController.java
+++ src/main/java/com/example/ProjectDodo/LoginController.java
@@ -2,6 +2,11 @@ package com.example.ProjectDodo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
@@ -9,5 +14,27 @@ public class LoginController {
     @GetMapping("/login")
     public String getLogin() {
         return "login";
+    }
+/*
+    @PostMapping("/login")
+    public String postForm(@RequestParam(name = "username") String username, @RequestParam(name = "password") String password, HttpServletRequest request) {
+        Members member = loginRepository.getUser(username, password);
+        if (member == null) {
+            return "login";
+        } else if (username.equals(member.getUsername()) && password.equals(member.getPassword())) {
+            HttpSession session = request.getSession(true);
+            session.setAttribute("loggedIn", true);
+            return "redirect:welcome";
+        }
+        return "login";
+    }
+*/
+    @GetMapping("/home")
+    public String secret(HttpServletRequest request) {
+        HttpSession session = request.getSession(true);
+        if (session.getAttribute("loggedIn") != null)
+            return "home";
+        else
+            return "redirect:login";
     }
 }
